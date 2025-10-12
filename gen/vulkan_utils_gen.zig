a1473c199019e4eb02eb11d9076cd24be120069c
ShadowCurse <shadowcurse@noreply.codeberg.org>
2025-10-12 14:35:47 +0100

    gen: update check_result generation to use parsed vulkan Database instead of hardcoded values

--- gen/vulkan_utils_gen.zig
+++ gen/vulkan_utils_gen.zig
@@ -1408,56 +1408,7 @@ pub fn gen() !void {
     try write_chain_size(alloc, &file, &CHAIN_TYPES);
     _ = arena.reset(.retain_capacity);
 
-    try write_check_result(alloc, &file, &.{
-        "VK_NOT_READY",
-        "VK_TIMEOUT",
-        "VK_EVENT_SET",
-        "VK_EVENT_RESET",
-        "VK_INCOMPLETE",
-        "VK_ERROR_OUT_OF_HOST_MEMORY",
-        "VK_ERROR_OUT_OF_DEVICE_MEMORY",
-        "VK_ERROR_INITIALIZATION_FAILED",
-        "VK_ERROR_DEVICE_LOST",
-        "VK_ERROR_MEMORY_MAP_FAILED",
-        "VK_ERROR_LAYER_NOT_PRESENT",
-        "VK_ERROR_EXTENSION_NOT_PRESENT",
-        "VK_ERROR_FEATURE_NOT_PRESENT",
-        "VK_ERROR_INCOMPATIBLE_DRIVER",
-        "VK_ERROR_TOO_MANY_OBJECTS",
-        "VK_ERROR_FORMAT_NOT_SUPPORTED",
-        "VK_ERROR_FRAGMENTED_POOL",
-        "VK_ERROR_UNKNOWN",
-        "VK_ERROR_OUT_OF_POOL_MEMORY",
-        "VK_ERROR_INVALID_EXTERNAL_HANDLE",
-        "VK_ERROR_FRAGMENTATION",
-        "VK_ERROR_INVALID_OPAQUE_CAPTURE_ADDRESS",
-        "VK_PIPELINE_COMPILE_REQUIRED",
-        "VK_ERROR_NOT_PERMITTED",
-        "VK_ERROR_SURFACE_LOST_KHR",
-        "VK_ERROR_NATIVE_WINDOW_IN_USE_KHR",
-        "VK_SUBOPTIMAL_KHR",
-        "VK_ERROR_OUT_OF_DATE_KHR",
-        "VK_ERROR_INCOMPATIBLE_DISPLAY_KHR",
-        "VK_ERROR_VALIDATION_FAILED_EXT",
-        "VK_ERROR_INVALID_SHADER_NV",
-        "VK_ERROR_IMAGE_USAGE_NOT_SUPPORTED_KHR",
-        "VK_ERROR_VIDEO_PICTURE_LAYOUT_NOT_SUPPORTED_KHR",
-        "VK_ERROR_VIDEO_PROFILE_OPERATION_NOT_SUPPORTED_KHR",
-        "VK_ERROR_VIDEO_PROFILE_FORMAT_NOT_SUPPORTED_KHR",
-        "VK_ERROR_VIDEO_PROFILE_CODEC_NOT_SUPPORTED_KHR",
-        "VK_ERROR_VIDEO_STD_VERSION_NOT_SUPPORTED_KHR",
-        "VK_ERROR_INVALID_DRM_FORMAT_MODIFIER_PLANE_LAYOUT_EXT",
-        "VK_ERROR_FULL_SCREEN_EXCLUSIVE_MODE_LOST_EXT",
-        "VK_THREAD_IDLE_KHR",
-        "VK_THREAD_DONE_KHR",
-        "VK_OPERATION_DEFERRED_KHR",
-        "VK_OPERATION_NOT_DEFERRED_KHR",
-        "VK_ERROR_INVALID_VIDEO_STD_PARAMETERS_KHR",
-        "VK_ERROR_COMPRESSION_EXHAUSTED_EXT",
-        "VK_INCOMPATIBLE_SHADER_BINARY_EXT",
-        "VK_PIPELINE_BINARY_MISSING_KHR",
-        "VK_ERROR_NOT_ENOUGH_SPACE_KHR",
-    });
+    try write_check_result(alloc, &file, &db);
     _ = arena.reset(.retain_capacity);
     try write_extension_type(alloc, &file, &db);
     _ = arena.reset(.retain_capacity);
@@ -1993,7 +1944,7 @@ fn write_chain_size(
 fn write_check_result(
     alloc: Allocator,
     file: *const std.fs.File,
-    errors: []const []const u8,
+    db: *const vkp.Database,
 ) !void {
     _ = try file.write(
         \\pub fn check_result(result: vk.VkResult) !void {
@@ -2001,18 +1952,50 @@ fn write_check_result(
         \\        vk.VK_SUCCESS => return,
         \\
     );
-    for (errors) |err| {
-        const line = try std.fmt.allocPrint(
+    const e = db.enum_by_name("VkResult").?;
+    for (e.items) |*item| {
+        if (std.mem.eql(u8, item.name, "VK_SUCCESS")) continue;
+        try write_fmt(
+            file,
             alloc,
-            \\        vk.{s} => {{
-            \\            log.err(@src(), "Vulkan error: {s}", .{{}});
+            \\        vk.{[name]s} => {{
+            \\            log.err(@src(), "Vulkan error: {[name]s} ({[comment]s})", .{{}});
             \\            return error.{s};
             \\        }},
             \\
         ,
-            .{ err, err, err },
+            .{ .name = item.name, .comment = item.comment },
         );
-        _ = try file.write(line);
+    }
+    var used_enums: std.StringArrayHashMapUnmanaged(void) = .empty;
+    var iter = db.all_extensions();
+    while (iter.next()) |tuple| {
+        const ext, _ = tuple;
+        for (ext.require) |*req| {
+            for (req.items) |item| {
+                switch (item) {
+                    .@"enum" => |ee| {
+                        const enum_name = if (ee.alias) |a| a else ee.name;
+                        if (std.mem.eql(u8, ee.extends, "VkResult")) {
+                            if (used_enums.getPtr(enum_name) != null) continue;
+                            try used_enums.put(alloc, enum_name, {});
+                            try write_fmt(
+                                file,
+                                alloc,
+                                \\        vk.{[name]s} => {{
+                                \\            log.err(@src(), "Vulkan error: {[name]s} ({[extension]s})", .{{}});
+                                \\            return error.{s};
+                                \\        }},
+                                \\
+                            ,
+                                .{ .name = enum_name, .extension = ext.name },
+                            );
+                        }
+                    },
+                    else => {},
+                }
+            }
+        }
     }
     _ = try file.write(
         \\        else => {
