4547120960d9e82745051f4412d0cf147609a79c
ShadowCurse <shadowcurse@noreply.codeberg.org>
2025-12-18 20:39:34 +0000

    misc: specify MESA_SHADER_CACHE in the env to avoid poluting global cache

--- shell.nix
+++ shell.nix
@@ -3,6 +3,7 @@ pkgs.mkShell {
   VULKAN_INCLUDE_PATH = "${pkgs.lib.makeIncludePath [pkgs.vulkan-headers]}";
   VK_LAYER_PATH = "${pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d";
   LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath [pkgs.vulkan-loader]}";
+  MESA_SHADER_CACHE_DIR = "${builtins.getEnv "PWD"}/shader_cache";
 
   buildInputs = with pkgs; [
     vulkan-tools
